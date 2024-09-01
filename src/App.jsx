4f9d62dbd17685607d90ecb869c56cf076493bec
Anuj Chauhan <166821850+anujchauhann09@users.noreply.github.com>
2024-09-01 07:15:39 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* <Route path='/about' element={<About />} /> */}
+        </Routes>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </Router>
   )
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <nav className="nav w-full">
+            <div className="navbar-container w-full bg-slate-400 flex p-3">
+                <div className="left-nav w-1/2">
+                    <span id="logo-name">CourseMaterialExchange</span>
+                </div>
+                <div className="right-nav w-1/2">
+                    right
+                </div>
+            </div>
+        </nav>
+    )
+}
